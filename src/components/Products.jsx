56b26f8b1613c34cf8c3a3d458aa38462a1b4b3f
Nicolas Pavon <nicosubealanube@gmail.com>
2022-10-23 18:16:34 -0300

    useContext al checkout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
 import AppContext from '../context/AppContext';
 import '../styles/components/Products.css';
@@ -12,14 +12,14 @@ const Products = () => {
   }
 
   return (
-    <div className='Products'>
-        <div className="Products-items">
-            {products.map(product => (
-                <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
-            ))}
-        </div>
+    <div className="Products">
+      <div className="Products-items">
+        {products.map(product => (
+          <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Products;
